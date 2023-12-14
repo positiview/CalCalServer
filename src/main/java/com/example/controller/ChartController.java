3a92fd3a5faa9e53bf2db8f90b5973247bae281b
JOO0507 <wlduswn12@gmail.com>
2023-12-14 17:14:51 +0900

    도넛 그래프 완료_ 운동 한기록 추가되면 수정해야함

--- src/main/java/com/example/controller/ChartController.java
+++ src/main/java/com/example/controller/ChartController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import com.example.repository.MemberRepository;
 import com.example.service.ChartService;
 import com.example.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +13,24 @@ import java.util.Map;
 @Controller
 public class ChartController {
     private final ChartService chartService;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public ChartController(ChartService chartService) {
+    public ChartController(ChartService chartService, MemberRepository memberRepository) {
+
         this.chartService = chartService;
+        this.memberRepository = memberRepository;
     }
+
 
     @GetMapping("/dashboard")
     public String showChart(Model model) {
+        long totalMembers = memberRepository.count();
+
+        // Add total member count to the model
+        model.addAttribute("totalMembers", totalMembers);
+        Map<String, Long> loginCounts = chartService.getLoginCounts();
+        model.addAttribute("loginCounts", loginCounts);
         Map<String, Long> genderCounts = chartService.getGenderCounts();
         model.addAttribute("genderCounts", genderCounts);
         Map<String, Long> ageGroupCounts = chartService.getAgeGroupCounts();
--- src/main/java/com/example/service/ChartService.java
+++ src/main/java/com/example/service/ChartService.java
@@ -4,6 +4,7 @@ package com.example.service;
 import com.example.entity.MemberEntity;
 import com.example.model.MemberDTO;
 import com.example.repository.MemberRepository;
+import com.example.repository.RouteRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,13 +19,31 @@ import java.util.TreeMap;
 public class ChartService {
 
     private MemberRepository memberRepository;
+    private RouteRecordRepository routeRecordRepository;
 
     @Autowired
-    public ChartService(MemberRepository memberRepository) {
+    public ChartService(MemberRepository memberRepository, RouteRecordRepository routeRecordRepository) {
         this.memberRepository = memberRepository;
+        this.routeRecordRepository = routeRecordRepository;
     }
 
-
+    public Map<String, Long> getLoginCounts() {
+        List<MemberEntity> members = memberRepository.findAll();
+        long countLoggedIn = 0;
+        long countNotLoggedIn = 0;
+        for (MemberEntity member : members) {
+            String email = member.getEmail();
+            if (routeRecordRepository.existsByEmail(email)) {
+                countLoggedIn++;
+            } else {
+                countNotLoggedIn++;
+            }
+        }
+        Map<String, Long> loginMap = new HashMap<>();
+        loginMap.put("접속한 회원", countLoggedIn);
+        loginMap.put("접속하지 않은 회원", countNotLoggedIn);
+        return loginMap;
+    }
 
     public Map<String, Long> getGenderCounts() {
         List<Object[]> genderCounts = memberRepository.countByGender();
